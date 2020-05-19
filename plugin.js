8d0a3f092534fee5274dfb0365651507372c96bb
GTaRR <mr.sushiloff@yandex.ru>
2020-05-19 12:26:55 +0300

    Добавление возможности использовать this.$metrika в компонентах.

--- plugin.js
+++ plugin.js
@@ -7,19 +7,37 @@ export default ({ app: { router } }, inject) => {
   })
 
   function create() {
-    window['yaCounter<%= options.id %>'] = new Ya.Metrika(<%= JSON.stringify(options) %>)
-    router.afterEach((to, from) => {
-      if (!ready) {
-        // Don't record a duplicate hit for the initial navigation.
-        return
-      }
-      window['yaCounter<%= options.id %>'].hit(to.fullPath, {
-        referer: from.fullPath
-        // TODO: pass title: <new page title>
-        // This will need special handling because router.afterEach is called *before* DOM is updated.
+    if (this.options.dev && process.env.NODE_ENV !== 'production') {
+      window['yaCounter<%= options.id %>'] = new Ya.Metrika( <%= JSON.stringify(options) %>
+    )
+      router.afterEach((to, from) => {
+        if (!ready) {
+          // Don't record a duplicate hit for the initial navigation.
+          return
+        }
+        window['yaCounter<%= options.id %>'].hit(to.fullPath, {
+          referer: from.fullPath
+          // TODO: pass title: <new page title>
+          // This will need special handling because router.afterEach is called *before* DOM is updated.
+        })
       })
-    })
-    inject('metrika', window['yaCounter<%= options.id %>'])
+      inject('metrika', window['yaCounter<%= options.id %>'])
+    } else {
+      console.warn('[vue-yandex-metrika] Tracking is disabled, because env option is not "production"')
+      inject('metrika', {
+        addFileExtension() { console.log('[vue-yandex-metrika] addFileExtension:', arguments)},
+        extLink() { console.log('[vue-yandex-metrika] extLink:', arguments) },
+        file() { console.log('[vue-yandex-metrika] file:', arguments) },
+        getClientID() { console.log('[vue-yandex-metrika] getClientID:', arguments) },
+        hit() { console.log('[vue-yandex-metrika] hit:', arguments) },
+        notBounce() { console.log('[vue-yandex-metrika] notBounce:', arguments) },
+        params() { console.log('[vue-yandex-metrika] params:', arguments) },
+        reachGoal() { console.log('[vue-yandex-metrika] reachGoal:', arguments) },
+        replacePhones() { console.log('[vue-yandex-metrika] replacePhones:', arguments) },
+        setUserID() { console.log('[vue-yandex-metrika] setUserID:', arguments) },
+        userParams() { console.log('[vue-yandex-metrika] userParams:', arguments) }
+      })
+    }
   }
 
   if (window.Ya && window.Ya.Metrika) {
